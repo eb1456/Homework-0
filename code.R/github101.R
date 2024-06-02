06a5e60e9a6ee2df6152daf71c9834900c44a9dc
Ezra Baraban <ezra.baraban@gmail.com>
2024-06-01 20:46:58 -0400

    adding a new script

---
+++ code.R/github101.R
@@ -0,0 +1,29 @@
+#https://rafalab.dfci.harvard.edu/dsbook/accessing-the-terminal-and-installing-git.html#installing-git-and-git-bash-on-windows
+#installing git on windows
+
+#mac: cmd-spacebar and type terminal
+
+#check if installed by typing 'git --version'
+
+#R>tools>global options
+
+#create a new repo @ github
+
+#copy the URL to the repo, ie https://github.com/eb1456/Homework-0.git
+
+#in the terminal window w/i Rstudio > 
+
+#"git config --global user.name "Your Name""
+# ie git config --global user.name "Ezra Baraban"
+
+#git config --global user.email "ezra.baraban@gmail.com"
+
+#now start project that syncs w Git
+
+#new project > version ctl > github > paste URL from github
+
+#note 'git' tab in upper R
+
+#yellow means github website unaware of this file
+
+#to add file to github, check the stage box
