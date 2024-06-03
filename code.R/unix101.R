8e3449f163409076be0b582cbd7ab5d2e3c939f0
Ezra Baraban <ezra.baraban@gmail.com>
2024-06-02 21:27:09 -0400

    addition

--- code.R/unix101.R
+++ code.R/unix101.R
@@ -3,3 +3,27 @@
 # https://www.codecademy.com/learn/learn-the-command-line
 
 # echo "hello world"
+
+# pwd = print working dir;
+
+# / represents the ROOT directory;
+# When a path starts with "/", it is a "full path", which finds the current directory from the root directory
+
+# ~ represents the HOME/WORKING directory; ie ~/photos
+# ie cd ~ gives you the home directory vs cd / gives you ROOT
+
+# ls = list contents
+
+#mkdir and rmdir ie mkdir projects then rmdir projects, creates and removes EMPTY directories only
+
+#to remove real dir w contents -> rm
+
+#cd .. means go up a level, or can concatanate; ie cd ../.. takes you up 2 levels;
+
+#cd . means stay where you are, ie '.' = current dir;
+
+#cd - takes you back wherever you just were;
+
+#cd alone - takes you to home directory;
+
+
